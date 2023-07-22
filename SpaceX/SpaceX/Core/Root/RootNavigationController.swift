710e8946d89a0cdb3af3cda74d385d4e554ad504
Alexander <nefest71@gmail.com>
2023-07-23 00:04:35 +0300

    refactor: Отключена прозрачность и настроен NavBar в RootNavBar

--- SpaceX/SpaceX/Core/Root/RootNavigationController.swift
+++ SpaceX/SpaceX/Core/Root/RootNavigationController.swift
@@ -17,8 +17,20 @@ final class RootNavigationController: UINavigationController {
     override var prefersStatusBarHidden: Bool { true }
     
     private func configure() {
-        let textAttributes = [NSAttributedString.Key.foregroundColor: Colors.navigationBarTitle.uiColor]
-        navigationBar.titleTextAttributes = textAttributes
+        let navBarAppearance = UINavigationBarAppearance()
+        let buttonAppearance = UIBarButtonItemAppearance()
+        
+        let textAttributes: [NSAttributedString.Key: Any] = [.foregroundColor: Colors.navigationBarTitle.uiColor, .font: Fonts.textRootNavBar.uiFont]
+        
+        navBarAppearance.titleTextAttributes = textAttributes
+        buttonAppearance.normal.titleTextAttributes = textAttributes
+        
+        navBarAppearance.configureWithOpaqueBackground()
+        navBarAppearance.backgroundColor = Colors.backgroundNavigationBar.uiColor
+        navBarAppearance.buttonAppearance = buttonAppearance
+        
         navigationBar.tintColor = Colors.navigationBarTint.uiColor
+        navigationBar.standardAppearance = navBarAppearance
+        navigationBar.scrollEdgeAppearance = navBarAppearance
     }
 }
